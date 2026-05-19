c2186b9107dbd7890ebe9cdf47c465cbc806187c
incredible1986 <incredible@mail.ru>
2026-05-19 19:53:34 +0300

    Создана StudentEditViewModel с валидацией

---
+++ Models/StudentEditViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PersonalAccount.Models
+{
+    public class StudentEditViewModel
+    {
+        [Required(ErrorMessage = "Имя обязательно для заполнения")]
+        public string FullName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Группа обязательна для заполнения")]
+        public string GroupName { get; set; } = string.Empty;
+
+        public string? PhotoUrl { get; set; }
+    }
+}
