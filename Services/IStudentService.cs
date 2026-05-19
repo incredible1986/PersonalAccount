4fb92b0051c1501d5a7fe8f6e7f83c1fe93bbf27
incredible1986 <incredible@mail.ru>
2026-05-19 19:51:43 +0300

    Добавлены IStudentService и StudentService

---
+++ Services/IStudentService.cs
@@ -0,0 +1,11 @@
+﻿using PersonalAccount.Models;
+using PersonalAccount.Models.Students;
+
+namespace PersonalAccount.Services
+{
+    public interface IStudentService
+    {
+        Task<StudentModel?> GetByIdAsync(int id);
+        Task UpdateByIdAsync(int id, StudentEditViewModel model);
+    }
+}
---
+++ Services/StudentService.cs
@@ -0,0 +1,26 @@
+﻿using PersonalAccount.Models;
+using PersonalAccount.Models.Students;
+using PersonalAccount.Repositories;
+
+namespace PersonalAccount.Services
+{
+    public class StudentService(IStudentRepo<StudentModel> repo) : IStudentService
+    {
+        public async Task<StudentModel?> GetByIdAsync(int id)
+            => await repo.GetByIdAsync(id);
+
+        public async Task UpdateByIdAsync(int id, StudentEditViewModel model)
+        {
+            var student = await repo.GetByIdAsync(id)
+                ?? throw new InvalidOperationException($"Student with id {id} not found");
+
+            student.FullName = model.FullName;
+            student.GroupName = model.GroupName;
+            student.PhotoUrl = string.IsNullOrWhiteSpace(model.PhotoUrl)
+                ? null
+                : new Uri(model.PhotoUrl);
+
+            await repo.UpdateByIdAsync(id, student);
+        }
+    }
+}
