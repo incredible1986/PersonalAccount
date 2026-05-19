df42047047a875b646ad8549e9c8ecaf137e531d
incredible1986 <incredible@mail.ru>
2026-05-19 19:58:15 +0300

    Обновлён CabinetController: Index и Edit

--- Controllers/CabinetController.cs
+++ Controllers/CabinetController.cs
@@ -1,22 +1,54 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using PersonalAccount.Models;
 using PersonalAccount.Models.Students;
+using PersonalAccount.Services;
 using PersonalAccount.Utils;
 
 namespace PersonalAccount.Controllers;
 
 [Authorize]
-public class CabinetController : Controller
+public class CabinetController(IStudentService studentService) : Controller
 {
     [HttpGet]
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
         var studentId = User.GetId();
         if (studentId == null) return RedirectToAction("Error", "Home");
-        
-        return View(new StudentModel
+
+        var student = await studentService.GetByIdAsync(studentId.Value);
+        if (student == null) return RedirectToAction("Error", "Home");
+
+        return View(student);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Edit()
+    {
+        var studentId = User.GetId();
+        if (studentId == null) return RedirectToAction("Error", "Home");
+
+        var student = await studentService.GetByIdAsync(studentId.Value);
+        if (student == null) return RedirectToAction("Error", "Home");
+
+        return View(new StudentEditViewModel
         {
-            Id = studentId.Value,
+            FullName = student.FullName,
+            GroupName = student.GroupName,
+            PhotoUrl = student.PhotoUrl?.ToString()
         });
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(StudentEditViewModel model)
+    {
+        var studentId = User.GetId();
+        if (studentId == null) return RedirectToAction("Error", "Home");
+
+        if (!ModelState.IsValid) return View(model);
+
+        await studentService.UpdateByIdAsync(studentId.Value, model);
+        return RedirectToAction("Index");
     }
 }
