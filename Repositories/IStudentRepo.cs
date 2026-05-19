0a0c0bc15381c14b0fe4a4e342b0d1736fafbad9
incredible1986 <incredible@mail.ru>
2026-05-19 19:56:58 +0300

    Добавлены GetByIdAsync и UpdateByIdAsync в StudentRepo

--- Repositories/IStudentRepo.cs
+++ Repositories/IStudentRepo.cs
@@ -5,5 +5,7 @@ namespace PersonalAccount.Repositories
     public interface IStudentRepo<T> where T : StudentModel
     {
         public Task<T?> GetByEmailAsync(string email);
+        public Task<T?> GetByIdAsync(int id);
+        public Task UpdateByIdAsync(int id, T student);
     }
 }
--- Repositories/StudentRepo.cs
+++ Repositories/StudentRepo.cs
@@ -9,7 +9,27 @@ namespace PersonalAccount.Repositories;
 public class StudentRepo<T>(AppDbContext context, IMapper<StudentEntity, T> mapper) : IStudentRepo<T> where T : StudentModel
 {
     private DbSet<StudentEntity> Students => context.Students;
-    
+
+
+    public async Task<T?> GetByIdAsync(int id)
+    {
+        var entity = await Students
+            .AsNoTracking()
+            .FirstOrDefaultAsync(e => e.Id == id);
+        return mapper.ToModel(entity);
+    }
+
+    public async Task UpdateByIdAsync(int id, T student)
+    {
+        var entity = await Students.FindAsync(id)
+            ?? throw new InvalidOperationException($"Student with id {id} not found");
+
+        entity.FullName = student.FullName;
+        entity.GroupName = student.GroupName;
+        entity.PhotoUrl = student.PhotoUrl?.ToString();
+
+        await context.SaveChangesAsync();
+    }
     public async Task<T?> GetByEmailAsync(string email)
     {
         var entity = await Students
