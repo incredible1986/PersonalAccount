073aee099c41633a143bd2aee9af71d32419532d
incredible1986 <incredible@mail.ru>
2026-05-19 20:13:19 +0300

    Добавлена PasswordChangeViewModel с валидацией

---
+++ Models/PasswordChangeViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PersonalAccount.Models
+{
+    public class PasswordChangeViewModel
+    {
+        [Required(ErrorMessage = "Текущий пароль обязателен")]
+        [DataType(DataType.Password)]
+        public string OldPassword { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Новый пароль обязателен")]
+        [DataType(DataType.Password)]
+        [MinLength(4, ErrorMessage = "Пароль должен быть не менее 4 символов")]
+        public string NewPassword { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Подтверждение пароля обязательно")]
+        [DataType(DataType.Password)]
+        [Compare("NewPassword", ErrorMessage = "Пароли не совпадают")]
+        public string ConfirmNewPassword { get; set; } = string.Empty;
+    }
+}
