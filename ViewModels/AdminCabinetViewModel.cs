ebc938bcd90c0ae86d83d51a0cb8a39cc6aa1542
incredible1986 <incredible@mail.ru>
2026-05-25 03:03:27 +0300

    1.2.4

---
+++ ViewModels/AdminCabinetViewModel.cs
@@ -0,0 +1,13 @@
+﻿public class AdminCabinetStudentViewModel
+{
+    public string FullName { get; set; } = string.Empty;
+
+    public string GroupName { get; set; } = string.Empty;
+
+    public string? PhotoUrl { get; set; }
+}
+
+public class AdminCabinetViewModel
+{
+    public List<AdminCabinetStudentViewModel> Students { get; set; } = [];
+}
